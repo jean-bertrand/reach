471bf6151ebdb9b84aae9f6c55f14fde1bd5e79d
jean-bertrand <juwiling@gmail.com>
2018-10-10 03:38:12 -0400

    - set up page directory structure - add favicon and logo

---
+++ pages/user-guide/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        User Guide
+      </h1>
+      <div class="links">
+        <a
+          href="/terms"
+          class="button--grey">Terms of Use</a>
+        <a
+          href="/privacy"
+          class="button--grey">Privacy Policy</a>
+        <a
+          href="/user-guide"
+          class="button--grey">User Guide</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
