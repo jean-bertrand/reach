b3e2e57270eb9e55fc6fc7477715a3e8b8cbba4f
jean-bertrand <juwiling@gmail.com>
2018-10-10 09:38:25 -0400

    configure netlify cms settings

---
+++ content/user-guide/articles/setup-and-installation.md
@@ -0,0 +1,10 @@
+---
+layout: guide
+title: "Setup and Installation"
+date: 1999-12-31 11:59:59 -0800
+thumbnail: "/images/logo-color.svg"
+rating: 5
+slug: setup-and-installation
+---
+
+For now, Reach is accessible as a web app which you’ll use in your phone’s browser. Simply open your browser and navigate to app.reach.vote.
---
+++ pages/user-guide/_slug.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <article>
+    <h1>{{ params.title }}</h1>
+    <p>{{ params.content }}
+      <br>
+      <strong>Posted on <time :datetime="date">{{ params.date }}</time></strong>
+    </p>
+    <img :src="params.thumbnail">
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({params}) {
+    const fileContent = import(`~/content/user-guide/articles/${params.slug}.`)
+    return {
+      content: fileContent
+    }
+  }
+}
+</script>
